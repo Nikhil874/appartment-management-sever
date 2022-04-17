3437cc04893a665c56061ee1b4e63c776988011c
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-04-17 19:07:54 +0530

    deploy

--- src/controller/flatController.js
+++ src/controller/flatController.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express=require("express");
 const Flat=require("../model/flatModel");
 const router=express.Router();
@@ -25,9 +26,27 @@ router.get("/",async(req,res)=>{
     }
   
 })
+router.get("/residents/:id",async(req,res)=>{
+    try{
+        console.log("a")
+        const flat=await Flat.findOne({_id:req.params.id}).lean().exec();
+        
+        res.status(201).send(flat);
+        // console.log(flats)
+    }
+    catch(e){
+     console.log(e.message)
+    }
+  
+})
 router.get("/:_id",async(req,res)=>{
     try{
-        const flats=await Flat.find({managerId:req.params._id}).lean().exec();
+        let flats;
+      if(req.query.flatNo==1){
+         flats=await Flat.find({managerId:req.params._id}).sort({flatNo:1}).lean().exec();
+         console.log("sorted")
+      }
+         flats=await Flat.find({managerId:req.params._id}).lean().exec();
         res.status(201).send(flats);
         // console.log(flats)
     }
